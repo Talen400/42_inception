4ce98c9c22b84b59a1f7ee5e28f4369730b1d033
Talen400 <talengithub0@gmail.com>
2026-05-22 04:04:56 -0300

    feat(nginx): configure container entrypoint
    	-- (feat) Add Dockerfile, docker-compose configuration, and env file structure
    	-- (feat) Implement nginx_entrypoint.sh to dynamically generate SSL certificates via openssl
    	-- (feat) Fix server configuration path by routing the populated template to Alpine's default inclusion directory (/etc/nginx/http.d/)
    	-- (feat) Correct directive syntax typos (listel to listen) and ensure path consistency for self-signed certificates

---
+++ srcs/requirements/nginx/tools/nginx_entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Use from .env DOMAIN_NAME in openssl.conf.template
+envsubst '$DOMAIN_NAME' < /etc/nginx/ssl/openssl.conf.template > /etc/nginx/ssl/openssl.conf
+
+# Generate the certificate SSL using configuration from openssl.conf
+openssl req -config /etc/nginx/ssl/openssl.conf \
+	-new -x509 -sha256 -newkey rsa:2048 -nodes -batch \
+    -keyout /etc/nginx/ssl/nginx-selfsigned.key \
+	-days 365 -out /etc/nginx/ssl/nginx-selfsigned.crt
+
+# Use from .env DOMAIN_NAME in ngnix.conf.template
+envsubst '$DOMAIN_NAME' < /etc/nginx/http.d/default.conf.template > /etc/nginx/http.d/default.conf
+
+exec "$@"
